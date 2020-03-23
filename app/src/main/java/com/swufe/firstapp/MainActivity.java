6efa6de39a9a1b0aa4793c60fb08e17dc84df7a9
Sakura-llj <1715893699@qq.com>
2020-03-23 11:44:03 +0800

    Week4-TempConvert c2f

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -2,23 +2,45 @@ package com.swufe.firstapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText input;
+    private Button btn;
+    private TextView txtout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //通过findViewById方法获取控件
+        input = findViewById(R.id.input);
+        String str = input.getText().toString();
+
+        btn = findViewById(R.id.btn);
+        btn.setText("转换为华氏温度");
+        btn.setOnClickListener(this);
+
+        txtout = findViewById(R.id.txtout);
+        Log.i("main", "input=" + str);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("click", "onClick.....");
+
+        String str = input.getText().toString();
+        Double num = Double.valueOf(input.getText().toString());
+        Double result = num * 1.8 + 32;
+        txtout.setText("结果为：" + result);
     }
 }
-
