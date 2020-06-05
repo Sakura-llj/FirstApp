0282c74560ed82485eac1b7b9935aaf6bbbd44e0
Sakura-llj <1715893699@qq.com>
2020-06-05 20:22:09 +0800

    event processing(btn)_Current class listener

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -13,34 +13,33 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText input;
-    private Button btn;
-    private TextView txtout;
+    TextView out;
+    EditText inp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //通过findViewById方法获取控件
-        input = findViewById(R.id.input);
-        String str = input.getText().toString();
+        out = findViewById(R.id.showText);//TextVtew在上面import class
+        inp = findViewById(R.id.inpText);
 
-        btn = findViewById(R.id.btn);
-        btn.setText("转换为华氏温度");
-        btn.setOnClickListener(this);
+        Button btn = findViewById(R.id.btn1);//Button在上面import class,button要进行事件处理必须设置监听
+        btn.setOnClickListener(this);//btn.setOnClickListener()可以设置当前类监听，也可以设置匿名类监听,按下button时调用this对象的onClick方法
 
-        txtout = findViewById(R.id.txtout);
-        Log.i("main", "input=" + str);
+        //System.out.print("input" + str);Java中的输出处理
+        //Log.i("main","input" + str);//Log在上面import class,日志处理
     }
 
     @Override
     public void onClick(View v) {
-        Log.i("click", "onClick.....");
+        Log.i("click","onclick .....");
 
-        String str = input.getText().toString();
-        Double num = Double.valueOf(input.getText().toString());
-        Double result = num * 1.8 + 32;
-        txtout.setText("结果为：" + result);
+        //TextView tv = findViewById(R.id.showText);
+
+        //EditText inp = findViewById(R.id.inpText);
+        String str = inp.getText().toString();//获取用户输入，.toString是将获取的类型变成字符串
+
+        out.setText("Hello " + str);
     }
 }
